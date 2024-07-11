dd9489c3be8354834413cdfa2d35ec35d0185c02
AnnemarieSchmidt1806 <annemarie2005@gmx.de>
2024-07-11 09:11:54 +0200

    small changes and 0004 migration

--- lists/models.py
+++ lists/models.py
@@ -5,5 +5,5 @@ class List(models.Model):
 
 # Create your models here.
 class Item(models.Model):
-    text = models.TextField(default = "")
-    list = models.ForeignKey(List, default = None, on_delete = models.CASCADE)
+    text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
